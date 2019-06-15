31093ce0e4aadd2d4603120186f26a43733ec65f
Anon <anon@example.com>
2019-06-14 21:50:13 -0400

    Grape...

---
+++ mango/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nitinjenson/Documents/Web Devolpment/Django stuff/kiwi/mango/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
